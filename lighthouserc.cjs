d5ce32b80f08167c5eace7de016b08de351910cd
Jamie Craik <154467285+jscraik@users.noreply.github.com>
2026-02-16 17:24:26 +0000

    ci: add Lighthouse CI config to fix dev server startup

---
+++ lighthouserc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './dist',
+      url: ['http://localhost:4173'],
+      numberOfRuns: 3,
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
